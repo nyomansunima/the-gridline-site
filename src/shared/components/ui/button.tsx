d32688fdfec33944f854dd336456f42736d52573
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-04 23:33:06 +0800

    refactor: change button style

--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -5,7 +5,7 @@ import type { VariantProps } from "class-variance-authority"
 import { mergeClass } from "@shared/libs"
 
 const buttonVariants = cva(
-  "flex items-center justify-center font-medium whitespace-nowrap text-sm leading-none! transition-all duration-300 gap-2 group relative cursor-pointer select-none",
+  "flex items-center justify-center font-medium font-heading whitespace-nowrap text-sm leading-none! transition-all duration-300 gap-2 group relative cursor-pointer select-none",
   {
     variants: {
       variant: {
@@ -16,9 +16,9 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground border border-transparent hover:bg-secondary/60 hover:border-border",
       },
       size: {
-        base: "h-10 px-4 rounded-xl text-sm",
+        base: "h-10 px-5 rounded-full text-sm",
         sm: "h-9 px-3 rounded-full",
-        lg: "h-14 px-5 rounded-2xl",
+        lg: "h-12 px-6 rounded-full",
         icon: "h-10 w-10 rounded-2xl text-sm bg-surface",
       },
     },
