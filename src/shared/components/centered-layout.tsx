765a772b312a17b4e3cba53547e1432f37d4f8af
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-04 23:33:19 +0800

    fix: update center layout size

--- src/shared/components/centered-layout.tsx
+++ src/shared/components/centered-layout.tsx
@@ -5,7 +5,7 @@ interface CenteredLayoutProps {
 export function CenteredLayout({ children }: CenteredLayoutProps) {
   return (
     <div className="px-5 laptop:px-0 container mx-auto">
-      <div className="mx-auto w-full tablet:w-11/12 laptop:w-9/12 desktop:w-7/12 laptop:px-20">
+      <div className="mx-auto w-full tablet:w-11/12 laptop:w-9/12 desktop:w-7/12">
         {children}
       </div>
     </div>
