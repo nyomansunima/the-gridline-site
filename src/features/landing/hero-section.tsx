0be27748838898648e8ae35c532edca20e7b9535
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-06 18:57:14 +0800

    refactor: add link items

--- src/features/landing/hero-section.tsx
+++ src/features/landing/hero-section.tsx
@@ -1,6 +1,34 @@
-import { Button } from "@shared/components"
 import logo from "./logo.webp"
 import { Link } from "react-router"
+
+interface ItemData {
+  label: string
+  url: string
+}
+
+const items: ItemData[] = [
+  {
+    label: "Substack",
+    url: "https://thegridline.substack.com",
+  },
+  {
+    label: "Linkedin newsletter",
+    url: "https://www.linkedin.com/newsletters/the-gridline-7357803082438643713",
+  },
+]
+
+function Item({ item }: { item: ItemData }) {
+  return (
+    <Link
+      to={item.url}
+      target="_blank"
+      className="flex items-center gap-2 py-2"
+    >
+      <i className="fi fi-sc-bullet text-sm text-foreground/40" />
+      <span>{item.label}</span>
+    </Link>
+  )
+}
 
 export function HeroSection() {
   return (
@@ -19,18 +47,15 @@ export function HeroSection() {
       </h2>
 
       <p className="leading-7 mt-8 text-center text-pretty w-8/12 text-foreground/60">
-        A weekly dose of practical web design strategies. No fluff — just
-        clarity, craft, and conversion.
+        Top web designer communities. Help you understand web design better and
+        give you overall tips and tricks for running a web design business on
+        your own.
       </p>
 
-      <div className="flex items-center mt-10">
-        <Button
-          size={"lg"}
-          asChild
-          className="transition-all duration-300 hover:scale-95"
-        >
-          <Link to={"/#resources"}>Bring me in, now</Link>
-        </Button>
+      <div className="flex flex-col gap-1 mt-10">
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
       </div>
     </section>
   )
