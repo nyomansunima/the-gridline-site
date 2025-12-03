1efe56d3bde63b226db7680602fe0d8ef19b6f3c
Nyoman Sunima <nyomansunima@gmail.com>
2025-12-03 17:00:20 +0800

    refactor: remove utils

--- src/shared/libs/utils.ts
+++
@@ -1,7 +0,0 @@
-import type { ClassValue } from "clsx";
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function mergeClass(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
